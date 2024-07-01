ce4598dad3b8d71b095b261ab47c57dfc8167d4b
williamjs95 <williamj.silva@live.com>
2024-07-01 16:22:04 -0300

    chore: Update TypeScript and ESLint configurations

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,18 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    '@rocketseat/eslint-config/node',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    'camelcase': 'off',
+    'no-useless-constructor': 'off',
+    'indent': ['error', 2],
+    'no-tabs': 'error',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'off', 
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
---
+++ CLI.md
@@ -0,0 +1,5 @@
+>> to tun CSS in JS
+npm i styled-components
+
+>> types for styled-components
+npm i @types/styled-components -D
