906c0aaf4509d7b0caef80abd430c772a08e39e5
williamjs95 <williamj.silva@live.com>
2024-07-02 17:10:49 -0300

    chore: Add styled-components with default theme to Button component

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,6 +1,6 @@
-import styled, { css } from 'styled-components';
+import styled, { css } from 'styled-components'
 
-export type ButtonVariant = 'primary' | 'secondary' | 'success' | 'danger';
+export type ButtonVariant = 'primary' | 'secondary' | 'success' | 'danger'
 
 interface ButtonContainerProps {
   variant: ButtonVariant
@@ -10,7 +10,7 @@ const ButtonVariants = {
   primary: 'purple',
   secondary: 'orange',
   danger: 'red',
-  success: 'green'
+  success: 'green',
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
@@ -20,12 +20,12 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border: 0;
   margin: 8px;
 
-  background-color: ${props => props.theme.primary};
-  color: ${props => props.theme.white};
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
 
-  /* ${props => {
+  /* ${(props) => {
     return css`
-      background-color: ${ButtonVariants[props.variant]}
+      background-color: ${ButtonVariants[props.variant]};
     `
   }} */
 `
