007fcbabe1239050832c16dcfa2756d93880c1e9
williamjs95 <williamj.silva@live.com>
2024-07-03 16:25:36 -0300

    chore: Update Home page with countdown timer functionality

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { act, useState } from 'react'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
