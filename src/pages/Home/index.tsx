131c5f01dbb9038ad3f5132c738b26813bcb5a68
williamjs95 <williamj.silva@live.com>
2024-07-03 15:18:50 -0300

    chore: Reset form after creating new cycle in Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,7 +23,7 @@ const nweCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof nweCycleFormValidationSchema>
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(nweCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -33,6 +33,7 @@ export function Home() {
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
+    reset()
   }
 
   const task = watch('task')
