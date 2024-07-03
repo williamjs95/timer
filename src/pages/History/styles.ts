777b05e7b749a74bd7663d7c2135246f00a0eaf4
williamjs95 <williamj.silva@live.com>
2024-07-02 23:12:59 -0300

    chore: Add status indicators to History page

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const HistoryContainer = styled.main`
   flex: 1;
-  padding: 3.5;
+  padding: 2.5rem;
 
   display: flex;
   flex-direction: column;
@@ -58,5 +58,29 @@ export const HistoryList = styled.div`
     &:last-child {
       padding-right: 1.5rem;
     }
+  }
+`
+
+const STATUS_COLORS = {
+  yellow: 'yellow-500',
+  red: 'red-500',
+  green: 'green-500',
+} as const
+
+interface StatusProps {
+  statusColor: keyof typeof STATUS_COLORS
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 9999px;
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
   }
 `
